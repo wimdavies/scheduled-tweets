9b2bad854ab1373ca64f829606fc8cfed03b2b9a
wimdavies <william.johnston.davies@gmail.com>
2023-10-30 17:02:04 +0000

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   # uses bcrypt to encrypt passwords
   has_secure_password
 
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'invalid email address provided' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2023_10_30_155354) do
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
