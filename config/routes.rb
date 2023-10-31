c62e8c510e41fe0067ba07ecc92d17e023fe316b
wimdavies <william.johnston.davies@gmail.com>
2023-10-31 14:39:50 +0000

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
 
   get "sign_up", to: "registrations#new"
-
   post "sign_up", to: "registrations#create"
 
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
