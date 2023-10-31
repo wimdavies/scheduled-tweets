01fe77a0eddc4bd7ca4d55706691d26dc27fd349
wimdavies <william.johnston.davies@gmail.com>
2023-10-31 15:12:05 +0000

    Setup current user infrastructure for sessions, move login/out/signup to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  def set_current_user
+    if session[:user_id]
+      # `find_by` will not throw error if cookie user_id can't be found in db
+      #  e.g. user deletes their account, we don't want app to crash immediately
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,4 @@
 class MainController < ApplicationController
   def index
-    if session[:user_id]
-      # `find_by` will not throw error if cookie user_id can't be found in db
-      #  e.g. user deletes their account, we don't want app to crash immediately
-      @user = User.find_by(id: session[:user_id])
-    end
   end
 end
