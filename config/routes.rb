b3f0eb5cfc7e39fc9485ad6f130535c2a5fe5fb5
wimdavies <william.johnston.davies@gmail.com>
2023-10-31 14:15:22 +0000

    Add logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   post "sign_up", to: "registrations#create"
 
+  delete "logout", to: "sessions#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
